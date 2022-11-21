edb2d348962a33626817fbf7aa1cade6de062e86
mateuszwlo <mateuszwlodarczyk2002@gmail.com>
2022-11-21 18:10:28 +0000

    A* search implemented

--- ailp/library/wp_library.pl
+++ ailp/library/wp_library.pl
@@ -208,7 +208,7 @@ init_identity(A) :-
 % failure-driven loop to test all identities
 test :-
   init_identity(_A),  % initialise; don't reveal identity
-  user:find_identity(A),  % program under test
+  usere:find_identity(A),  % program under test
   ailp_identity(I),
   ( A=I -> writeln('I am':A)
   ; otherwise -> writeln('Wrong answer':A)
--- cw_2050732.pl
+++ cw_2050732.pl
@@ -1,15 +1,42 @@
 % Accomplish a given Task and return the Cost
 solve_task(Task,Cost) :-
-    my_agent(A), get_agent_position(A,P),
-    solve_task_dfs(Task,[P],[P|Path]), !,
-    agent_do_moves(A,Path), length(Path,Cost).
+    my_agent(A), 
+    get_agent_position(A,P),
+    (achieved(Task,P) -> true
+    ;otherwise   -> search_a_star(Task, [[arc(0,P)]], [], Path),
+                    agent_do_moves(A,Path),
+                    length(Path,Cost)).
 
-% Calculate the path required to achieve a Task
-solve_task_dfs(Task,[P|Ps],Path) :-
-    achieved(Task,P), reverse([P|Ps],Path)
-    ;
-    map_adjacent(P,Q,empty), \+ member(Q,Ps),
-    solve_task_dfs(Task,[Q,P|Ps],Path).
+search_a_star(Task,[Next|Rest],Visited,Path) :-
+    Next = [Pos|RPath],
+    Pos = arc(C,P),
+    (achieved(Task,P) -> reverse([Pos|RPath],[_|WPath]), formatPath(WPath, Path)
+    ;otherwise     -> (findall([arc(NC,NP),Pos|RPath],
+                               (map_adjacent(P,NP,empty), 
+                               length([Pos|RPath], G),
+                               manhattan_distance(NP, Task, H), NC is G + H,
+                               \+ member_of_list(arc(NC,NP),Rest),
+                               \+ member_of_list(arc(NC,NP),Visited)),
+                               Children),
+                      append(Rest,Children,NewQueue),
+                      sort(NewQueue, SortedQueue),
+                      search_a_star(Task,SortedQueue,[Pos|Visited],Path))).
+
+manhattan_distance(p(X,Y), go(p(X1,Y1)), H) :-
+    H is abs(X - X1) + abs(Y - Y1),!.
+manhattan_distance(_, _, H) :- H is 0.
+
+formatPath([], []).
+formatPath([arc(C,P)|T], [P|T1]) :- formatPath(T, T1).
+
+member_of_list(p(X,Y)), []) :- false.
+member_of_list(arc(C,p(X,Y)), [arc(C1,p(X1,Y1))|T]) :- 
+    X == X1, Y == Y1, C1 =< C
+    ; 
+    member_of_list(arc(C,p(X,Y)), T).
+
+print_list([]) :- nl.
+print_list([H|T]) :- write(H), write(", "), print_list(T).
 
 % True if the Task is achieved with the agent at Pos
 achieved(Task,Pos) :- 
