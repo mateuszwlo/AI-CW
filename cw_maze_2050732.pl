3cab46867c1a57feed2ed2c7ece9b93e63275b6f
mateuszwlo <mateuszwlodarczyk2002@gmail.com>
2022-12-06 03:58:24 +0000

    added comments to part 3

--- cw_maze_2050732.pl
+++ cw_maze_2050732.pl
@@ -5,29 +5,32 @@ solve_maze() :-
     convlist(get_agent_position, Agents, Visited),
     find_moves(Agents, Visited, []).
 
+% Find the best move for Agent A to make
 solve(A, Visited, Pivots, NVisisted, NPivots) :-
-    findall((W,p(X,Y)), (agent_adjacent(A,p(X,Y),empty), \+ member(p(X,Y),Visited), W is X + Y), AvailableMoves),
+% Find adjacent squares which no other agents have visited
+    findall((W,p(X,Y)),(agent_adjacent(A,p(X,Y),empty),\+ member(p(X,Y),Visited),W is X + Y),AvailableMoves),
     length(AvailableMoves, L),
     length(Pivots, PLen),
+% As the exit is at p(N,N), pick the square closest to the exit to explore first
+% Navigate to this square and add it to the visited list
     (L > 0 -> 
-        write("L > 0"),
-        nl,
         sort(AvailableMoves, SMoves),
         reverse(SMoves,Moves),
         nth1(1,Moves,(W,Pos)),    
         get_agent_position(A, P),
         agent_do_moves(A, [Pos]),
         append([Pos], Visited, NVisisted),
+% If there is more than 1 direction you can go from this current position, it is a pivot point 
+% Add this position to the pivot list, either this agent or another one will explore in the other direction 
     (L > 1 -> 
-        H is L - 1, append([pivot(H, P)], Pivots, NPivots), 
-        write("L > 1"), 
-        nl
+        H is L - 1, append([pivot(H, P)], Pivots, NPivots)
     ; otherwise -> NPivots = Pivots
     )
+% If Agent A has no available moves, visit the nearest pivot point to it
+% Some pivot points can have multiple directions to explore
+% Subtract 1 from the directions to explore counter and remove from the list if all directions have been explored
     ; otherwise -> 
-        write("L == 0 "),
-        nl,
-        (PLen == 0 -> NVisisted = Visited, NPivots = Pivots, write("PLen == 0"), nl
+        (PLen == 0 -> NVisisted = Visited, NPivots = Pivots
         ; otherwise -> 
             get_agent_position(A, Pos),
             (closest_pivot(Pos, Pivots, (K, Pivot, Path)) ->
@@ -41,6 +44,8 @@ solve(A, Visited, Pivots, NVisisted, NPivots) :-
         )
     ).
 
+% Find paths to each pivot point from Pos using search algorithm from part 1
+% Return information from the closest one
 closest_pivot(Pos, Pivots, (K, Pivot, Path)) :-
     convlist(path_to_pivot(Pos), Pivots, Paths),
     sort(Paths, SPaths),
@@ -51,22 +56,23 @@ path_to_pivot(Pos, pivot(K, PivotPosition), (L, K, Path)) :-
     search(Pos, go(PivotPosition), Path),
     length(Path, L).
 
+% Looped over all agents so continue looping over them
 find_moves([], Visited, Pivots) :- 
     my_agents(As), 
     reverse(As, Agents),
     find_moves(Agents, Visited, Pivots).
 
+% Iterates over all agents, find their best move and executes it
+% If exit is found by any agent, navigates the remaining ones to it
 find_moves([A|As], Visited, Pivots) :-
-    write("Agent "),
-    write(A),
-    nl,
-    write("Visited: "), print_list(Visited),
-    write("Pivots: "), print_list(Pivots),
-    (leave_maze(A) -> my_agents(Agents), go_to_exit(Agents), !
+    leave_maze(A) -> my_agents(Agents), go_to_exit(Agents), !
     ; otherwise -> 
     solve(A, Visited, Pivots, NVisisted, NPivots),
-    find_moves(As, NVisisted, NPivots)).
+    find_moves(As, NVisisted, NPivots).
 
+% Guide all agents to the exit when found
+% Find the path using search algorithm from part 1
+% If cant find path due to another agent blocking it, move onto the next agent and try again after it has been unblocked
 go_to_exit([]).
 go_to_exit([A|T]) :-
     get_agent_position(A, P),
@@ -77,6 +83,3 @@ go_to_exit([A|T]) :-
         go_to_exit(T)
     ; otherwise -> append(T, [A], T1), go_to_exit(T1)
     ).
-
-tail([H|T], T).
-head([H|T], H).
--- cw_wp_2050732.pl
+++ cw_wp_2050732.pl
@@ -2,8 +2,6 @@ actor_has_link(L,A) :-
 actor(A), wp(A,WT), wt_link(WT,L).
 
 eliminate(As,A,Oracles, Stations) :-
-    write("eliminate(As,A,Oracles, Stations)"),
-    nl,
     As=[A], !
     ;
     my_agent(N),
@@ -13,29 +11,18 @@ eliminate(As,A,Oracles, Stations) :-
     eliminate(As,A,Oracles, Stations, OPaths, SPaths).
 
 eliminate(As,A,Oracles,Stations,OPaths,SPaths) :-
-    write("eliminate(As,A,Oracles,Stations,OPaths,SPaths)"),
-    nl,
     my_agent(N),
     get_agent_energy(N, E),
-    get_agent_position(N, Pos),
-    write(Pos),
-    write(" "),
-    write(E),
-    nl,
     nth1(1, OPaths, (OLen, OraclePosition, OPath)),
     nth1(1, SPaths, (SLen, StationPosition, SPath)),
     (OLen == 0 -> get_agent_position(N, OP) 
     ; otherwise -> last(OPath, OP)),
     (should_topup(SLen, OLen, OP, Stations)-> 
-        write("topping up"),
-        nl,
         agent_do_moves(N, SPath),
         lookup_pos(StationPosition, c(K)),
         agent_topup_energy(N, c(K)),
         eliminate(As,A,Oracles,Stations)
     ; otherwise ->
-        write("going to oracle"),
-        nl,
         agent_do_moves(N, OPath),
         lookup_pos(OraclePosition, o(K)),
         agent_ask_oracle(N,o(K),link,L),
@@ -45,16 +32,12 @@ eliminate(As,A,Oracles,Stations,OPaths,SPaths) :-
     ).
 
 find_identity(A) :-
-    write("find_identity"),
-    nl,
     findall(A,actor(A),As), 
     find_all_oracles((Oracles, OPaths)),
     find_all_stations((Stations, SPaths)),
     eliminate(As,A,Oracles,Stations, OPaths, SPaths).
 
 should_topup(SLen, OLen, OraclePosition, Stations) :-
-    write("should_topup"),
-    nl,
     SLen \= 0,
     my_agent(N),
     get_agent_energy(N,E),
@@ -70,8 +53,6 @@ should_topup(SLen, OLen, OraclePosition, Stations) :-
     H > E.
 
 find_all_stations((Stations, SPaths)) :-
-    write("find_all_stations((Stations, SPaths))"),
-    nl,
     my_agent(A),
     get_agent_position(A,Pos),
     convlist(search_for_node([[Pos]], []), [c(1),c(2),c(3),c(4)], List),
@@ -79,20 +60,14 @@ find_all_stations((Stations, SPaths)) :-
     get_node_list(SPaths, Stations).
 
 get_paths_to_stations(Pos, Stations, Paths) :-
-    write("get_paths_to_stations(Pos, Stations, Paths)"),
-    nl,
     convlist(path_to_station(Pos), Stations, Ps),
     sort(Ps, Paths).
 
 path_to_station(Pos, StationPosition, (L, StationPosition, Path)) :-
-    write("path_to_station(Pos, StationPosition, (L, StationPosition, Path))"),
-    nl,
     search_bf_node([[Pos]], [], StationPosition,Path),
     length(Path, L).
 
 find_all_oracles((Oracles, SPaths)) :-
-    write("find_all_oracles((Oracles, SPaths))"),
-    nl,
     my_agent(A),
     get_agent_position(A,Pos),
     convlist(search_for_node([[Pos]], []), [o(1),o(2),o(3),o(4),o(5),o(6),o(7),o(8),o(9),o(10)], List),
@@ -100,18 +75,10 @@ find_all_oracles((Oracles, SPaths)) :-
     get_node_list(SPaths, Oracles).
 
 get_paths_to_oracles(Pos, Oracles, Paths) :-
-    write("get_paths_to_oracles(Pos, Oracles, Paths)"),
-    nl,
     convlist(path_to_oracle(Pos), Oracles, Ps),
     sort(Ps, Paths).
 
 path_to_oracle(Pos, OraclePosition, (L, OraclePosition, Path)) :-
-    write("path_to_oracle("),
-    write(Pos),
-    write(","),
-    write(OraclePosition),
-    write(", (L, OraclePosition, Path))"),
-    nl,
     search_bf_node([[Pos]], [], OraclePosition, Path),
     length(Path, L).
 
